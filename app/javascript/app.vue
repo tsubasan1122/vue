a6d9e9877bd881179bfbeee2dadff57ca4e12d88
tsubasan1122 <tsubasan1122@gmail.com>
2018-10-17 07:26:46 +0000

    to_do_app

---
+++ app/javascript/app.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      // この文字列が画面に表示されている
+      message: "Hello Vue!"
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
